505eb0baba3e620323934b3f88c7829f9c519144
Joseph Autorino <staircasesoul@gmail.com>
2021-04-15 00:16:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function increaseRankBy(n) {
   for (let i = 0, l = rankedLists.length; i < l; i++) {
     let children = rankedLists[i].children;
 
-    for (let i = 0, l = rankedLists.length; j < k; j++) {
+    for (let j = 0, k = children.length; j < k; j++) {
       children[j].innerHTML = parseInt(children[j].innerHTML) + n;
     }
   }

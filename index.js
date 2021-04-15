f55f698c0bed8e75cc94aae10a95ee5f3756af06
Joseph Autorino <staircasesoul@gmail.com>
2021-04-15 00:15:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function deepestChild() {
 }
 
 function increaseRankBy(n) {
-  const rankedLists = document.querySelectorAll('ranked-list');
+  const rankedLists = document.querySelectorAll('.ranked-list');
 
   for (let i = 0, l = rankedLists.length; i < l; i++) {
     let children = rankedLists[i].children;
